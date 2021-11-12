a438f5759df7baaa2c59eb019824c44418a5934e
Luís Henrique Braga de Sousa <agarb@pop-os.localdomain>
2021-11-12 14:26:10 -0300

    criação de todo o layout da página + bônus da tela incial

---
+++ script.js
@@ -0,0 +1,10 @@
+function menuContatosAparece(){
+    const menuAparece = document.querySelector(".menuContatos")
+    menuAparece.classList.remove("none");
+}
+
+function menuContatosDesaparece(){
+    const menuDesaparece = document.querySelector(".menuContatos")
+    menuDesaparece.classList.add("none");
+}
+
